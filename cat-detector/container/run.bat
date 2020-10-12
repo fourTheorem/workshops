3e786f4029ddd7e008cbfdd00526a4b7e375e59d
Peter Elger <elger.peter@gmail.com>
2020-10-12 13:50:49 +0100

    bat script

--- cat-detector/container/run.bat
+++ cat-detector/container/run.bat
@@ -1,20 +1,12 @@
 @ECHO OFF
-docker run -d --env-file=run.env --name workshop -p "127.0.0.1:8888:8888" -v "${WORKDIR}:/home/dev/work" pelger/aiasaservice
-sleep 10
-docker logs workshop
-docker exec -ti workshop /bin/bash
+setlocal
+set thisdir=%cd%
+for %%I in ("%thisdir%\..") do set "WORKDIR=%%~fI"
+echo %WORKDIR%
 
-
-
-
-@echo OFF
-
-set "variable=E:\myfiles\app1\data\file.csv"
-
-set "drive=%variable:~0,1%"
-
-set variable=%variable:~2%
-set "variable=%variable:\=/%"
+set "drive=%WORKDIR:~0,1%"
+set WORKDIR=%WORKDIR:~2%
+set "WORKDIR=%WORKDIR:\=/%"
 
 if %drive%==A set "drive=a"
 if %drive%==B set "drive=b"
@@ -22,21 +14,12 @@ if %drive%==C set "drive=c"
 if %drive%==D set "drive=d"
 if %drive%==E set "drive=e"
 if %drive%==F set "drive=f"
-if %drive%==G set "drive=g"
-if %drive%==H set "drive=h"
-if %drive%==I set "drive=i"
-if %drive%==J set "drive=j"
-if %drive%==K set "drive=k"
-if %drive%==L set "drive=l"
-if %drive%==M set "drive=m"
-if %drive%==N set "drive=n"
-if %drive%==O set "drive=o"
-if %drive%==P set "drive=p"
-if %drive%==Q set "drive=q"
 
+set "WORKDIR=/%drive%%WORKDIR%"
+echo %WORKDIR%
 
-set "variable=/mnt/%drive%%variable%"
+docker run -d --env-file=run.env --name workshop -p "127.0.0.1:8888:8888" -v "%WORKDIR%:/home/dev/work" pelger/aiasaservice
+sleep 10
+docker logs workshop
+docker exec -ti workshop /bin/bash
 
-echo "%variable%"
-
-@echo ON
--- cat-detector/container/run.cmd
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-docker run -d --env-file=run.env --name workshop -p "127.0.0.1:8888:8888" -v "${WORKDIR}:/home/dev/work" pelger/aiasaservice
-sleep 10
-docker logs workshop
-docker exec -ti workshop /bin/bash
